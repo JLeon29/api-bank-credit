a84c7fed1b501189b64cfc8efb9d89f9f5ffcaf9
Juan León <91449845+JLeon29@users.noreply.github.com>
2022-05-05 16:24:29 -0500

    Add files via upload

---
+++ api-bank-credit/src/main/java/com/nttdata/credit/dto/ClientTypeDto.java
@@ -0,0 +1,17 @@
+package com.nttdata.credit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientTypeDto {
+	
+	 private String name;
+	 private String code;
+
+}
